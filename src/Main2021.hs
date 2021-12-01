feb068ca724bc9b7a99b2366284f1acb0a41d59e
hanjoosten <han.joosten.han@gmail.com>
2021-12-01 21:46:29 +0100

    day 1 solution

--- src/Main2021.hs
+++ src/Main2021.hs
@@ -10,11 +10,21 @@ import Text.Read (Lexeme (String))
 
 
 getPuzzle :: ([String] -> String, FilePath)
-getPuzzle = from  puzzle24
+getPuzzle = from  puzzle1
    where from (a,b) = (a,show 2021 </> b) 
 
-puzzle24 :: ([String] -> String, FilePath)
-puzzle24 = (fun, "puzzle_1.txt")
+puzzle1 :: ([String] -> String, FilePath)
+puzzle1 = (fun, "puzzle_1.txt")
   where
-    fun rows = "Klaar voor de eerste puzzel"
+    fun rows = show . length . filter id . increases . sumOf3 . map toInt $ rows
+    toInt :: String -> Int
+    toInt = read
+    increases :: [Int] -> [Bool]
+    increases [] = []
+    increases [a] = []
+    increases (a:b:rest) = (a < b):increases (b:rest)
+    sumOf3 :: [Int] -> [Int]
+    sumOf3 xs = case xs of
+      (a:b:c:rest) -> a+b+c : sumOf3 (b:c:rest)
+      _ -> []
 
